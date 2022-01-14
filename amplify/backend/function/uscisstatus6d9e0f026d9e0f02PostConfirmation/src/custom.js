37bd47360a4225f25fca5e9c6ea8208560ba685a
takarocks <taka.kitazume@gmail.com>
2022-01-13 23:24:59 -0500

    Cognito Post Confirmation Lambda adding registered to Users group

--- amplify/backend/function/uscisstatus6d9e0f026d9e0f02PostConfirmation/src/custom.js
+++ amplify/backend/function/uscisstatus6d9e0f026d9e0f02PostConfirmation/src/custom.js
@@ -1,37 +1,25 @@
-var aws = require('aws-sdk')
-//var ddb = new aws.DynamoDB()
+var aws = require('aws-sdk');
+var cidp = new aws.CognitoIdentityServiceProvider();
 
-exports.handler = async (event, context) => {
+exports.handler = async (event) => {
 
-    console.log(event)
-/* 
-    let date = new Date()
     if (event.request.userAttributes.sub) {
         let params = {
-            Item: {
-                'id': {S: event.request.userAttributes.sub},
-                '__typename': {S: 'User'},
-                'username': {S: event.userName},
-                'email': {S: event.request.userAttributes.email},
-                'createdAt': {S: date.toISOString()},
-                'updatedAt': {S: date.toISOString()},
-            },
-            TableName: process.env.USERTABLE
+            'UserPoolId': event.userPoolId,
+            'Username': event.userName,
+            'GroupName': 'Users'
         }
 
         try {
-            await ddb.putItem(params).promise()
+            await cidp.adminAddUserToGroup(params).promise()
             console.log("Success")
         } catch (err) {
             console.log("Error", err)
         }
 
-        console.log("Success: Everything executed correctly")
-        context.done(null, event)
-
+        console.log("Event processed")
     } else {
-        console.log("Error: Nothing was written to DynamoDB")
-        context.done(null, event)
+        console.log("Error: User was not added to Users group")
     }
-     */
+
 };
